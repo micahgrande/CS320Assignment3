1a71aea735d50a9d01c856d635c7b8a1c80b66f3
Micah Grande <micah.grande@gmail.com>
2017-11-23 05:30:55 +0000

    Update prog3_1.cpp

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -1,17 +1,21 @@
-#include <lua.hpp>
-#include <lauxlib.h>
-#include <lualib.h>
-#include <string.h>
-#include <stdio.h>
+    #include <stdio.h>
+    #include <string.h>
+    #include <lua.h>
+    #include <lauxlib.h>
+    #include <lualib.h>
+    
+    int main (int argc, char *argv[]) {
+      char buff[256];
+      int error;
 
-int main ( int argc, char *argv[] ) {
+      printf("Assignment #3-1, Micah Joseph Grande, micah.grande@gmail.com\n");
 
-    printf("Assignment #3-1, Micah Joseph Grande, micah.grande@gmail.com\n");
+      lua_State *L = luaL_newstate();
+      luaL_openlibs(L); 
 
-    lua_State *L = luaL_newState();
-    luaL_openlibs(L);
-    luaL_dofile(L, argv[1]);
+      luaL_dofile(L, argv[1]);
 
-    lua_close(L);
-    return 0;
-}
+      lua_close(L);
+      return 0;
+    }
+
