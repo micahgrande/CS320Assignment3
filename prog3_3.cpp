b8f58e59776abf4ae373cea47869d4c08e59e823
Micah Grande <micah.grande@gmail.com>
2017-11-23 05:42:12 +0000

    Update prog3_3.cpp

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -4,16 +4,34 @@
 #include <string.h>
 #include <stdio.h>
 
-int main ( in argc, char *argv[] ) {
+extern"C"
+{
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
 
-    printf("Assignment #3-1, Micah Joseph Grande, micah.grande@gmail.com\n");
+using namespace std;
+
+int main ( in argc, char *argv[] ) {
+    
+    char buff[256];
+    string itp = "return InfixToPostfix(\"";
+    string input;
+    
+    cout<<"Assignment #3-1, Micah Joseph Grande, micah.grande@gmail.com\n";
 
     lua_State *L;
     L = luaL_newState();
     luaL_openlibs(L);
     luaL_dofile(L, argv[1]);
+    getline(cin,input);
+    buff += end;
+    
+    strcpy(buff, itp.c_str());
+    luaL_dostring(L,buff);
+    cout << luaL_checkstring(L,1) << end;
 
-    lua_pcall(
 
     lua_close(L);
     return 0;
