2decd84e71b56a094a07d6b7ddcfe39682d6bbda
Micah Grande <micah.grande@gmail.com>
2017-11-23 05:29:55 +0000

    Update prog3_1.cpp

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -8,8 +8,7 @@ int main ( int argc, char *argv[] ) {
 
     printf("Assignment #3-1, Micah Joseph Grande, micah.grande@gmail.com\n");
 
-    lua_State *L;
-    L = luaL_newState();
+    lua_State *L = luaL_newState();
     luaL_openlibs(L);
     luaL_dofile(L, argv[1]);
 
