bb0b26562b469848b8fa23de8169d70f72f1ba14
Micah Grande <micah.grande@gmail.com>
2017-11-23 05:55:35 +0000

    Update prog3_3.cpp

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -16,21 +16,23 @@ using namespace std;
 
 int main ( int argc, char *argv[] ) {
     
-    char buff[256];
-    string itp = "return InfixToPostfix(\"";
-    string input;
     
     cout<<"Assignment #3-3, Micah Joseph Grande, micah.grande@gmail.com\n";
 
-    lua_State *L = luaL_newState();
+    lua_State *L = luaL_newstate();
     luaL_openlibs(L);
-    luaL_dofile(L, argv[1]);
-    getline(cin,input);
-    buff += end;
+    lua_dofile(L,argv[1]);
     
-    strcpy(buff, itp.c_str());
-    luaL_dostring(L,buff);
-    cout << luaL_checkstring(L,1) << end;
+    string buff = "return InfixToPostfix(\"")
+    string input;
+    getline(cin,input);
+    buff +=input;
+    string end = "\")";
+    buff+=end;
+    char test[1024];
+    strcpy(test,buff.c_str());
+    luaL_dostring(L,test);
+    cout << luaL_checkstring(L,1) <<end;
 
 
     lua_close(L);
